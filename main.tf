92db886488549d7713cbcd151bceb5d523107a89
owlfred <23skdu>
2019-09-20 21:26:38 -0700

    update

---
+++ main.tf
@@ -0,0 +1,21 @@
+resource "virtualbox_vm" "node" {
+  count     = 3 
+  name      = format("node-%02d", count.index + 1)
+  image     = "virtualbox-centos7.7.1908_kube.box"
+  cpus      = 1 
+  memory    = "2048 mib"
+  user_data = file("user_data")
+
+  network_adapter {
+    type           = "hostonly"
+    host_interface = "vboxnet1"
+  }
+}
+
+output "IPAddr" {
+  value = element(virtualbox_vm.node.*.network_adapter.0.ipv4_address, 1)
+}
+
+output "IPAddr_2" {
+  value = element(virtualbox_vm.node.*.network_adapter.0.ipv4_address, 2)
+}
---
+++ scripts/teardown.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -eux
+
+#zero out rest of free space
+dd if=/dev/zero of=/EMPTY bs=1M
+rm -f /EMPTY
+
+# Add `sync` so Packer doesn't quit too early, before the large file is deleted.
+sync
