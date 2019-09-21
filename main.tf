12c7b43f743b445ea507a3487fb5e6262e27e505
owlfred <23skdu>
2019-09-21 07:55:10 -0700

    updating NIC

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 resource "virtualbox_vm" "node" {
   count     = 3 
-  name      = format("node-%02d", count.index + 1)
-  image     = "virtualbox-centos7.7.1908_kube.box"
+  name      = format("kube-%02d", count.index + 1)
+  image     = "virtualbox-centos7.7.1908-kube.box" 
   cpus      = 1 
   memory    = "2048 mib"
 
   network_adapter {
-    type           = "hostonly"
-    host_interface = "vboxnet1"
+    type           = "bridged"
+    host_interface = "enp0s25"
   }
 }
 
@@ -18,3 +18,8 @@ output "IPAddr" {
 output "IPAddr_2" {
   value = element(virtualbox_vm.node.*.network_adapter.0.ipv4_address, 2)
 }
+
+output "IPAddr_2" {
+  value = element(virtualbox_vm.node.*.network_adapter.0.ipv4_address, 3)
+}
+
