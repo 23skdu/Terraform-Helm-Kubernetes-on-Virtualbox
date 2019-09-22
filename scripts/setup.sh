d95c1de622b446714dac179e315c75b24321863d
owlfred <23skdu>
2019-09-21 22:22:06 -0700

    making sure guest additions gets installed by packer

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,3 +1,11 @@
 #!/bin/bash -eux
-yum update -y
-yum clean all
+sudo yum update -y
+sudo yum install -y bzip2 make perl kernel-devel
+sudo yum -y --enable-repo=epel install dkms
+sudo systemctl start dkms
+sudo systemctl enable dkms
+sudo mount -o loop,ro ~/VBoxGuestAdditions.iso /mnt/
+sudo /mnt/VBoxLinuxAdditions.run || :
+sudo umount /mnt/
+rm -f ~/VBoxGuestAdditions.iso
+sudo yum clean all
